f6cb04564366f241410a6c7f3fe5f032e3deaed9
anshrv <123404198+anshrv@users.noreply.github.com>
2023-10-24 15:49:09 +0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+const menuButton = document.querySelector(".m-menu");
+const menu = document.querySelector(".menu");
+menuButton.addEventListener('click', () => {
+    menu.classList.toggle("is-open");
+  });
